10b99a6d96a985ffe9952600fa99e2328d482914
inbal <inbal729@gmail.com>
2019-05-28 16:39:28 +0300

    anbn working

---
+++ anbn.py
@@ -0,0 +1,26 @@
+import numpy as np
+import state_machine as sm
+
+z = (np.pi) * np.sqrt(2)
+
+u_a = np.array([[np.cos(z), -np.sin(z), 0, 0],
+                [np.sin(z), np.cos(z), 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1]])
+
+
+u_b = np.array([[np.cos(-z), -np.sin(-z), 0, 0],
+                [np.sin(-z), np.cos(-z), 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1]])
+
+init_state = [[1],[0],[0],[0]]
+receiving_state = [[1],[0],[0],[0]]
+word = np.array([0,1,0,1,0,0,0]) #example word
+dicti = {0: u_a, 1: u_b}
+num_states = 2
+
+state_machine = sm.StateMachine(dicti, num_states, init_state, receiving_state)
+
+result = state_machine.transfer(word)
+print(result)
